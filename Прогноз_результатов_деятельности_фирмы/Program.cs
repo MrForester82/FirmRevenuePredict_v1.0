feb784df8ff17302354e765d433e42579d5a4775
MrForester82 <potate82@ya.ru>
2017-03-30 22:14:53 +0300

    Добавьте файлы проекта.

---
+++ Прогноз_результатов_деятельности_фирмы/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Прогноз_результатов_деятельности_фирмы
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
